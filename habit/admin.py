94a11febdeb81f4a15eabb903f45f6aef7c43846
panthurr0 <olapopaola@mail.ru>
2024-07-02 16:35:22 +0300

    edit HabitAdmin

--- habit/admin.py
+++ habit/admin.py
@@ -5,4 +5,10 @@ from habit.models import Habit
 
 @admin.register(Habit)
 class HabitAdmin(admin.ModelAdmin):
-    list_filter = ('id', 'action', 'is_active', 'is_public')
+    list_display = (
+        'id',
+        'action',
+        'is_active',
+        'is_public'
+    )
+    list_filter = ('id',)
