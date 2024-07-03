3da4e5923a3b5226714c78431919eddc055bb953
panthurr0 <olapopaola@mail.ru>
2024-07-03 19:12:49 +0300

    some flake8 fixes

--- habit/urls.py
+++ habit/urls.py
@@ -1,15 +1,40 @@
 from django.urls import path
 from habit.apps import HabitConfig
-from habit.views import (HabitCreateApiView, HabitListApiView,
-                         HabitRetrieveApiView, HabitUpdateApiView, HabitDestroyApiView)
+from habit.views import (
+    HabitCreateApiView,
+    HabitListApiView,
+    HabitRetrieveApiView,
+    HabitUpdateApiView,
+    HabitDestroyApiView
+)
 
 app_name = HabitConfig.name
 
 urlpatterns = [
-    path('habit/create', HabitCreateApiView.as_view(), name='Habit-create'),
-    path('habit/', HabitListApiView.as_view(), name='Habit-list'),
-    path('habit/<int:pk>', HabitRetrieveApiView.as_view(), name='Habit-detail'),
-    path('habit/update/<int:pk>', HabitUpdateApiView.as_view(), name='Habit-update'),
-    path('habit/delete/<int:pk>', HabitDestroyApiView.as_view(), name='Habit-delete'),
+    path(
+        'habit/create',
+        HabitCreateApiView.as_view(),
+        name='Habit-create'
+    ),
+    path(
+        'habit/',
+        HabitListApiView.as_view(),
+        name='Habit-list'
+    ),
+    path(
+        'habit/<int:pk>',
+        HabitRetrieveApiView.as_view(),
+        name='Habit-detail'
+    ),
+    path(
+        'habit/update/<int:pk>',
+        HabitUpdateApiView.as_view(),
+        name='Habit-update'
+    ),
+    path(
+        'habit/delete/<int:pk>',
+        HabitDestroyApiView.as_view(),
+        name='Habit-delete'
+    ),
 
 ]
