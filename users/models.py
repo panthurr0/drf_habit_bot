90cbed9bd9392cfec1e5937d724c69a2863e3077
panthurr0 <olapopaola@mail.ru>
2024-07-05 14:45:38 +0300

    add tg_id to user model

--- users/models.py
+++ users/models.py
@@ -1,37 +1,25 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-NULLABLE = {'blank': True, 'null': True}
+NULLABLE = {"blank": True, "null": True}
 
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(
-        verbose_name='почта', unique=True
-    )
+    email = models.EmailField(verbose_name="Почта", unique=True)
     phone_number = models.CharField(
-        verbose_name='номер телефона',
-        max_length=30,
-        **NULLABLE
+        verbose_name="Номер телефона", max_length=30, **NULLABLE
     )
-    avatar = models.ImageField(
-        verbose_name='аватар',
-        upload_to='users/',
-        **NULLABLE
-    )
-    city = models.CharField(
-        verbose_name='город',
-        max_length=100,
-        **NULLABLE
-    )
-    tg_id =
+    avatar = models.ImageField(verbose_name="Аватар", upload_to="users/", **NULLABLE)
+    city = models.CharField(verbose_name="Город", max_length=100, **NULLABLE)
+    tg_id = models.CharField(verbose_name="ID в телегераме", max_length=50, **NULLABLE)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     def __str__(self):
-        return f'{self.email}'
+        return f"{self.email}"
 
     class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
